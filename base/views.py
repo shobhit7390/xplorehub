6260795f118e7cb8d56200eb103cf7e3c0b09d84
shobhit7390 <vms6402@gmail.com>
2023-04-28 10:50:23 +0530

    Chat room crud functionality

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Room,Topic
+from .models import Room,Topic,Message
 from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
@@ -84,7 +84,19 @@ def home(request):
 
 def room(request,pk):
     room=Room.objects.get(id=pk)
-    context={'room':room}
+    room_messages=room.message_set.all().order_by('-created')
+    participants=room.participants.all()
+
+    if request.method == 'POST':
+        message=Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    
+    context={'room':room, 'room_messages':room_messages, 'participants':participants}
     return render(request,'base/room.html',context)
 
 
@@ -130,3 +142,16 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect("home")
     return render(request,'base/delete.html', {'obj':room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message=Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed to delete anyones\'s else room!!')
+    
+    if request.method=="POST":
+        message.delete()
+        return redirect("home")
+    return render(request,'base/delete.html', {'obj':message})
