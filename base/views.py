15ef632cf537db6cbb10f5f4544c15821fcf7ce8
shobhit7390 <vms6402@gmail.com>
2023-05-01 01:42:08 +0530

    Editing user account page

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Room,Topic,Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -176,3 +176,17 @@ def deleteMessage(request,pk):
         message.delete()
         return redirect("home")
     return render(request,'base/delete.html', {'obj':message})
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user=request.user
+    form=UserForm(instance=user)
+
+    if request.method == 'POST':
+        form=UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    return render(request, 'base/update-user.html', {'form':form})
