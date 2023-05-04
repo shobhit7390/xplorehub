04908796b77ca8fc6ba2e0c666a1fdbe8048a47a
shobhit7390 <vms6402@gmail.com>
2023-05-05 00:51:57 +0530

    Integrating custom User Model

--- xplorehub/settings.py
+++ xplorehub/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+AUTH_USER_MODEL='base.User'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
@@ -124,11 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL= '/images/'
 
 STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT=BASE_DIR / 'static/images'
 
 # STATIC_ROOT=
 
